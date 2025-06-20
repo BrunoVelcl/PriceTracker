f90812b08e0c8fb1f2b1f74ca783284a4bfa7328
Bruno <brunovelcl@gmail.com>
2025-06-20 05:14:53 +0200

    add Unzipper class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        LinkFetcher k = new LinkFetcher();
-        k.getLinksLidl();
-        Downloader download = new Downloader();
-        download.download(null);
+        //LinkFetcher k = new LinkFetcher();
+        //k.getLinksLidl();
+        //Downloader download = new Downloader();
+        //download.download(null);
+
+        Unzipper.unzipAllInDir();
 
     }
 }
---
+++ src/main/java/Unzipper.java
@@ -0,0 +1,40 @@
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.zip.*;
+
+public class Unzipper {
+    //scans the target dir unpacks all .zip files, and deletes the archives
+    public static void unzipAllInDir(String dir){
+
+        File cwd = new File(dir);
+        File[] names = cwd.listFiles();
+        if(names == null){
+            return;
+        }
+
+        for(File file : names){
+            String filename = file.getName();
+            if (file.getName().contains(".zip")){
+                try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(Path.of(dir + filename)))){
+                    ZipEntry entry;
+                    while ((entry = zis.getNextEntry()) != null){
+                        Files.copy(zis, Path.of(dir + entry.getName()), StandardCopyOption.REPLACE_EXISTING);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Failed to create input stream");
+                    return;
+                }
+                if(!file.delete()){
+                    System.err.println("Unzipping process couldn't delete file: " + filename);
+                }
+                System.out.println("\u001b[32mUNZIPED\u001b[37m: " + filename);
+            }
+        }
+    }
+    public static void unzipAllInDir(){
+        unzipAllInDir("G:\\Dev\\Prices\\dumpster\\");
+    }
+}
